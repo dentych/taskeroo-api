86c4e6f3a8b0c695a7a8b466c76fd1ee25cf141b
dentych <dennis@tychsen.me>
2022-02-20 20:06:11 +0100

    Add index to protected routes

--- internal/app.go
+++ internal/app.go
@@ -49,7 +49,7 @@ func Run() {
 
 	protectedRouter := router.Group("")
 	protectedRouter.Use(authMiddleware(auth))
-	router.GET("/", func(ctx *gin.Context) {
+	protectedRouter.GET("/", func(ctx *gin.Context) {
 		//render with master
 		ctx.HTML(http.StatusOK, "pages/index", gin.H{
 			"title": "Index title!",
