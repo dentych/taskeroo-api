c639b8d69072903d76c34373af44ee47e766e558
dentych <dennis@tychsen.me>
2022-02-22 23:03:32 +0100

    Fix notification sending

--- internal/app/notification.go
+++ internal/app/notification.go
@@ -3,14 +3,10 @@ package app
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/dentych/taskeroo/internal/database"
 	internalerrors "github.com/dentych/taskeroo/internal/errors"
 	"gorm.io/gorm"
-	"io"
 	"log"
-	"net/http"
-	"strings"
 )
 
 type NotificationLogic struct {
@@ -70,44 +66,8 @@ func (n *NotificationLogic) GetNotificationInfo(ctx context.Context, userID stri
 }
 
 func (n *NotificationLogic) SendNotification(ctx context.Context, userID string, msg string) error {
-	user, err := n.userRepo.Get(ctx, userID)
-	if err != nil {
-		return err
-	}
-	if user.GroupID == nil {
-		return internalerrors.ErrUserNotInGroup
-	}
-
-	groupDiscord, err := n.notificationRepo.GetGroupDiscord(ctx, *user.GroupID)
-	if err != nil {
-		return err
-	}
-	if groupDiscord == nil {
-		return nil
-	}
-
-	discordUsername, err := n.notificationRepo.GetDiscordUsername(ctx, userID)
-	if err != nil {
-		return err
-	}
-	if discordUsername == nil {
-		return nil
-	}
-	msg = fmt.Sprintf("{\"content\": \"Hej <@%s>!\\n%s\"}", discordUsername.DiscordID, msg)
-	req, err := http.NewRequest(http.MethodPost, groupDiscord.Webhook, strings.NewReader(msg))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode >= 300 {
-		body, _ := io.ReadAll(resp.Body)
-		log.Printf("Sending notification to user=%s, status code not successful: %d. Body: %s\n", userID, resp.StatusCode, string(body))
-	}
-	return nil
+	log.Printf("Sending notification to user=%s, msg:\n%s\n", userID, msg)
+	return n.telegramLogic.SendMessage(ctx, userID, msg)
 }
 
 func (n *NotificationLogic) NotifyAllInGroup(ctx context.Context, groupID string, msg string) error {
@@ -117,7 +77,7 @@ func (n *NotificationLogic) NotifyAllInGroup(ctx context.Context, groupID string
 	}
 
 	for _, user := range users {
-		err = n.telegramLogic.SendMessage(ctx, user.ID, msg)
+		err = n.SendNotification(ctx, user.ID, msg)
 		if err != nil {
 			log.Printf("Failed to send message to a member of group=%s, user=%s: %s", groupID, user.ID, err)
 		}
