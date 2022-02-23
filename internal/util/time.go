eb0c29f07ad0defabf8b92c8326f6a278676d525
dentych <dennis@tychsen.me>
2022-02-23 21:26:07 +0100

    New time and check that both hour and minute is smaller than current time

--- internal/util/time.go
+++ internal/util/time.go
@@ -4,8 +4,8 @@ import "time"
 
 func DurationToNextNoon(now time.Time) time.Duration {
 	hour := 20
-	minute := 25
-	if now.Hour() < hour {
+	minute := 30
+	if now.Hour() <= hour && now.Minute() < minute {
 		return time.Date(now.Year(), now.Month(), now.Day(), hour, minute, 0, 0, now.Location()).Sub(now)
 	} else {
 		tomorrow := now.AddDate(0, 0, 1)
