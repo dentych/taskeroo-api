b23292b0de00ee0c8e59ee05d11e20f93ace8d32
dentych <dennis@tychsen.me>
2022-02-23 20:57:47 +0100

    Add test, and set scheduler to run at 21 + make log statements when waiting and when running

--- internal/app/scheduler.go
+++ internal/app/scheduler.go
@@ -29,11 +29,14 @@ func (s *Scheduler) Start() {
 func (s *Scheduler) noonTask() {
 	for {
 		durationUntilTomorrowNoon := util.DurationToNextNoon(time.Now())
+		log.Printf("SCHEDULER: Waiting till noon. Duration in hours: %f\n", durationUntilTomorrowNoon.Hours())
 		time.Sleep(durationUntilTomorrowNoon)
 
+		log.Printf("SCHEDULER: Running notify tasks due today")
 		err := s.taskLogic.NotifyTasksDueToday(s.context)
 		if err != nil {
 			log.Printf("ERROR: NoonTask: Error during notification of tasks due today: %s", err)
 		}
+		log.Printf("SCHEDULER: Done running notify tasks due today")
 	}
 }
--- internal/util/time.go
+++ internal/util/time.go
@@ -3,10 +3,11 @@ package util
 import "time"
 
 func DurationToNextNoon(now time.Time) time.Duration {
-	if now.Hour() < 12 {
-		return time.Date(now.Year(), now.Month(), now.Day(), 12, 0, 0, 0, now.Location()).Sub(now)
+	hour := 21
+	if now.Hour() < hour {
+		return time.Date(now.Year(), now.Month(), now.Day(), hour, 0, 0, 0, now.Location()).Sub(now)
 	} else {
 		tomorrow := now.AddDate(0, 0, 1)
-		return time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), 12, 0, 0, 0, tomorrow.Location()).Sub(now)
+		return time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), hour, 0, 0, 0, tomorrow.Location()).Sub(now)
 	}
 }
