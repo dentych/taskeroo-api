aca3aa18ff1bfa0bdcbde1645b45a6c23bd13919
dentych <dennis@tychsen.me>
2022-02-23 21:10:05 +0100

    Set new run time for scheduler

--- internal/util/time.go
+++ internal/util/time.go
@@ -3,11 +3,12 @@ package util
 import "time"
 
 func DurationToNextNoon(now time.Time) time.Duration {
-	hour := 21
+	hour := 20
+	minute := 15
 	if now.Hour() < hour {
-		return time.Date(now.Year(), now.Month(), now.Day(), hour, 0, 0, 0, now.Location()).Sub(now)
+		return time.Date(now.Year(), now.Month(), now.Day(), hour, minute, 0, 0, now.Location()).Sub(now)
 	} else {
 		tomorrow := now.AddDate(0, 0, 1)
-		return time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), hour, 0, 0, 0, tomorrow.Location()).Sub(now)
+		return time.Date(tomorrow.Year(), tomorrow.Month(), tomorrow.Day(), hour, minute, 0, 0, tomorrow.Location()).Sub(now)
 	}
 }
