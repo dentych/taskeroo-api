e3fe5ee1d2a81397e30bdfc49f4054d4d0bd6ecf
dentych <dennis@tychsen.me>
2022-02-23 21:19:28 +0100

    New run time

--- internal/util/time.go
+++ internal/util/time.go
@@ -3,8 +3,8 @@ package util
 import "time"
 
 func DurationToNextNoon(now time.Time) time.Duration {
-	hour := 20
-	minute := 15
+	hour := 21
+	minute := 25
 	if now.Hour() < hour {
 		return time.Date(now.Year(), now.Month(), now.Day(), hour, minute, 0, 0, now.Location()).Sub(now)
 	} else {
