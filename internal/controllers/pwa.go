ecdcd08bd34bd80df16318fabdf39fa7116c508f
dentych <dennis@tychsen.me>
2022-02-24 22:07:23 +0100

    Updated sw.js

--- internal/controllers/pwa.go
+++ internal/controllers/pwa.go
@@ -54,7 +54,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      return fetch(event.request);
     })
   );
 });
