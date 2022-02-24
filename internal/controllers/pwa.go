f8f1dd88ec1366549742a8a7eb3234977f390e38
dentych <dennis@tychsen.me>
2022-02-24 22:15:24 +0100

    Add check for get request

--- internal/controllers/pwa.go
+++ internal/controllers/pwa.go
@@ -52,11 +52,13 @@ self.addEventListener('install', function(event) {
 });
  
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return fetch(event.request);
-    })
-  );
+  if (event.request.method == 'GET') {
+	event.respondWith(
+	  caches.match(event.request).then(function(response) {
+	    return fetch(event.request);
+	  })
+    );
+  }
 });
 `
 
